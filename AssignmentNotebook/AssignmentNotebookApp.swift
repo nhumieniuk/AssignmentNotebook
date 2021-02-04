58f1d6eeef7ce4b663ee5a3c516fde0ea502284e
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-02-04 10:29:52 -0600

    Initial Commit

---
+++ AssignmentNotebook/AssignmentNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebookApp.swift
+//  AssignmentNotebook
+//
+//  Created by Student on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentNotebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AssignmentNotebook
+//
+//  Created by Student on 2/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
