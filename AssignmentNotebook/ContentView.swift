0b587d4dab958d120ee4979fb99a64a33c84d7a8
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-02-11 10:56:56 -0600

    added an assignment list with deletion and ability to move

--- AssignmentNotebook/ContentView.swift
+++ AssignmentNotebook/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var AssignmentItems = ["asdsad", "asdasd", "akljalkf" ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(AssignmentItems, id: \.self) { thing in
+                    Text(thing)
+                }
+                .onMove(perform: { indices, newOffset in
+                    AssignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    AssignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+
+            .navigationBarTitle("AssignmentItems")
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+struct AssignmentItem: Identifiable, Codable {
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
+
+    
+
