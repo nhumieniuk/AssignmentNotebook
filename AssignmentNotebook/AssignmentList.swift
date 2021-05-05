e8367e4fe37e0a351434cc0957c9fb232addc037
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-05-05 09:06:49 -0500

    finished

---
+++ AssignmentNotebook/AssignmentList.swift
@@ -0,0 +1,27 @@
+//
+//  AddView.swift
+//  AssignmentNotebook
+//
+//  Created by Student on 5/5/21.
+//
+
+import Foundation
+
+class AssignmentList: ObservableObject {
+    @Published var items : [Assignment] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {  UserDefaults.standard.set(encoded, forKey: "data")  }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {  let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Assignment].self, from:  items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
+}
--- AssignmentNotebook/ContentView.swift
+++ AssignmentNotebook/ContentView.swift
@@ -6,37 +6,45 @@
 //
 
 import SwiftUI
-
-
 struct ContentView: View {
-    @State var AssignmentItems = ["asdsad", "asdasd", "akljalkf" ]
+    @ObservedObject var assignmentList = AssignmentList()
+    @State private var showingAddView = false
     var body: some View {
         NavigationView {
             List {
-                ForEach(AssignmentItems, id: \.self) { thing in
-                    Text(thing)
+                ForEach(assignmentList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.course)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
                 }
                 .onMove(perform: { indices, newOffset in
-                    AssignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                    assignmentList.items.move(fromOffsets: indices, toOffset:  newOffset)
                 })
                 .onDelete(perform: { indexSet in
-                    AssignmentItems.remove(atOffsets: indexSet)
-                })
+                            assignmentList.items.remove(atOffsets: indexSet)  })
             }
-
-            .navigationBarTitle("AssignmentItems")
-            .navigationBarItems(leading: EditButton())
+            .sheet(isPresented: $showingAddView, content: {  AddView(assignmentList: assignmentList)
+            })
+            .navigationBarTitle("Assignment List")
+            .navigationBarItems(leading: EditButton(), trailing: Button(action:{
+                                                                            showingAddView = true}) {  Image(systemName: "plus")  })
         }
     }
 }
-
-
-struct AssignmentItem: Identifiable, Codable {
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+struct Assignment: Identifiable, Codable {
     var id = UUID()
     var course = String()
     var description = String()
     var dueDate = Date()
 }
-
-    
-
